94c8df201b461ce025aa1dbb9f9090b9ba817f45
Dane Neves <dane@tadpull.com>
2021-04-10 21:27:16 -0700

    responsive percentage values completed and switch button added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   title = 'hackathon-todos';
   displayNewTodo: boolean = false;
   darkModeEnabled: boolean = false;
+  responsiveViewEnabled: boolean = true;
 
   onNewTodo(){
     this.displayNewTodo = !this.displayNewTodo;
@@ -16,5 +17,15 @@ export class AppComponent {
 
   exitNewTodo() {
     this.displayNewTodo = false;
+  }
+
+  styleGridView() {
+    //{'width': responsiveViewEnabled ? 'calc(128vh - 20px)' : '1102px'; 
+    //'height': responsiveViewEnabled ? 'calc(100vh - 20px)' : '860px'}
+
+    if (this.responsiveViewEnabled) {
+      return {width: 'calc(128vh - 20px)', height: 'calc(100vh - 20px)'};
+    }
+    return {width: '1102px', height: '860px'};
   }
 }
